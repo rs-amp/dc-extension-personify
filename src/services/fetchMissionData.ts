269002740b12c53c83d47f19d8ecc3debcf23f26
Shelley Weeks <sweeks@amplience.com>
2021-02-19 16:46:56 +0000

    feat: TM-20 confines retry to certain status codes

--- src/services/fetchMissionData.ts
+++ src/services/fetchMissionData.ts
@@ -1,34 +1,27 @@
+import { Criteria } from '../components/ManagedCriteria';
+import ResponseError from '../models/HttpError';
 import { stripTrailingSlash } from '../utils/stripTrailingSlash';
-export interface NetworkError extends Error {
-  status?: number;
-}
 
-export async function fetchMissionData(apiUrl: string | undefined, missions: string[] = [], tags: string[] = []) {
+export async function fetchMissionData(apiUrl: string | undefined, { tags = [], missions = [] }: Criteria) {
   if (!apiUrl) {
     throw new Error('Personify API required to be set in `apiUrl` installation parameter');
   }
-  return fetch(`${stripTrailingSlash(apiUrl)}/missiondata`, {
+
+  const response: Response = await fetch(`${stripTrailingSlash(apiUrl)}/missiondata`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({
-      missions,
-      tags,
-    }),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        const error: NetworkError = new Error('Unable to fetch mission data');
-        error.status = response.status;
-        throw error;
-      }
-      return response.json();
-    })
-    .then((response) => {
-      if (!response.missions || !response.tags) {
-        throw new Error(`Personify API response missing data: ${JSON.stringify(response)}`);
-      }
-      return response;
-    });
+    body: JSON.stringify({ tags, missions }),
+  });
+
+  const data = await response.json();
+  if (response.ok) {
+    const { tags, missions } = data;
+    if (!missions || !tags) {
+      throw new Error(`Personify API response missing data: ${JSON.stringify({ tags, missions })}`);
+    }
+    return data;
+  }
+  throw new ResponseError(response.status, data);
 }
--- src/utils/withRetry.ts
+++ src/utils/withRetry.ts
@@ -1,12 +1,13 @@
 function isRetryable({ status }: { status: number }): boolean {
-  return status !== undefined && status >= 400 && status < 600 && status !== 410;
+  return status !== undefined && status >= 400 && status < 600;
 }
+
 export async function withRetry<T>(promiseFn: () => Promise<T>, label: string = '', attempts: number = 3): Promise<T> {
   let lastError = null;
   for (let i = 0; i < attempts; i++) {
     try {
       const result = await promiseFn();
-      console.log(`Retrying. Attempt ${i} ${label}`);
+      console.log(`Attempt ${i}: ${label}`);
       return result;
     } catch (err) {
       if (!isRetryable(err)) {
